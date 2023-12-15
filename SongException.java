37a24b62d2e53b170c6a32b53b58b3b5a716c8d6
Elijah Tran Inofinada <102901526+eino808@users.noreply.github.com>
2023-12-14 23:08:54 -1000

    Update SongException.java

--- SongException.java
+++ SongException.java
@@ -1,2 +1,29 @@
-public class SongException {
+/**
+ * A custom exception for the Song object.
+ * It will be thrown if there is an attempt to create
+ * an invalid Song object. 
+ */
+
+public class SongException extends Exception {
+   
+   /** This message that outputs when the exception happens */
+   private String message = "";
+   
+   /**
+    * Constructs an exception
+    */
+    public SongException() {
+      // Empty Constructor
+    }
+    
+    /** Accessor/get Method */
+    public String getMessage() {
+      return this.message;
+    }
+    
+    /** Mutator/set Method */
+    public void setMessage(String newMessage) {
+      this.message = newMessage;
+    }
+   
 }
